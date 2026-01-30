d0d110c2dbaa1d8fe8c214fd5c6482163eb17e1a
fr0gydev <ljh130334@naver.com>
2026-01-30 12:14:27 +0900

    feat: Pretendard 폰트 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        pretendard: ['Pretendard', 'system-ui', 'sans-serif'],
+      },
       colors: {
         primary: {
           50: '#f0fdf4',
